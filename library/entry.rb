578b92fda715e19af153daa93a0240a77683c21b
LeotendoDev <leotendosteinhoff@gmail.com>
2022-03-03 16:18:48 +0100

    library/math.rb :: Implement basic math operations

---
+++ library/entry.rb
@@ -0,0 +1,20 @@
+
+def sum(num1, num2)
+ num1 + num2
+end
+
+def sub(num1, num2)
+  num1 - num2
+end
+
+def mul(num1, num2)
+  num1 * num2
+end
+
+def div(num1, num2)
+  num1 / num2
+end
+
+def squared(num)
+  num**2
+end
